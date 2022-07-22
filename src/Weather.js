3ad98a30e212498d8ee65ecdc7708be3ad7436cb
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-22 16:06:28 +1000

    Redid Code-
    Still functional

--- src/Weather.js
+++ src/Weather.js
@@ -1,78 +1,58 @@
-import React from "react";
-
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 80,
-    wind: 10,
-  };
+  <h2>Weather Search</h2>;
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeatherData] = useState({});
 
-  return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
+  function showTemperature(response) {
+    setLoaded(true);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      windSpeed: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "7bcf0da6ca80b20c501d86d32cc003a7";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a City" onChange={updateCity} />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h2>{city}</h2>
         <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />{" "}
+          </li>
+          <li> Temperature: {Math.round(weather.temperature)} degrees </li>
+          <li> Humidity: {weather.humidity} % </li>
+          <li> Wind Speed: {weather.windSpeed} km/hr </li>
+          <li> {weather.description} </li>
         </ul>
       </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
-      </div>
-      <br></br>
-      <footer>
-        Open Sourced code on
-        <a href="https://app.netlify.com/sites/weather-react-prac/overview">
-          Github
-        </a>
-        <br></br>
-        <a href="https://strigenz-portfolio.netlify.app/">Portfolio Here</a>
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    return form;
+  }
 }
